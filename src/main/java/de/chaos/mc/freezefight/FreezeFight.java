b0a3d07d1831f295f3621bf251292e22cdc6e5d6
Chaoshero5567 <79978761+Chaoshero5567@users.noreply.github.com>
2021-12-06 17:25:28 +0100

    doing shit

--- src/main/java/de/chaos/mc/freezefight/FreezeFight.java
+++ src/main/java/de/chaos/mc/freezefight/FreezeFight.java
@@ -1,80 +1,86 @@
 package de.chaos.mc.freezefight;
 
-import de.chaos.mc.freezefight.commands.setSpawnLocaton;
-import de.chaos.mc.freezefight.listeners.ConnectionListener;
-import de.chaos.mc.freezefight.listeners.DeathListener;
-import de.chaos.mc.freezefight.listeners.EventListener;
-import de.chaos.mc.freezefight.listeners.ProjectileListener;
-import de.chaos.mc.freezefight.utils.locationlibary.LocationInterface;
-import de.chaos.mc.freezefight.utils.locationlibary.LocationRepository;
 import de.chaos.mc.freezefight.utils.stats.StatsInterface;
 import de.chaos.mc.freezefight.utils.stats.StatsRepository;
 import de.chaos.mc.serverapi.api.ServerAPI;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-import java.util.Random;
+import java.io.File;
+import java.io.IOException;
 
+public class FreezeFight extends JavaPlugin implements Listener {
+	private static ServerAPI serverAPI;
+	private StatsRepository statsRepository;
+	private static StatsInterface statsInterface;
+	public static File locations;
+	public static FileConfiguration loc;
+	
+	public void onEnable() {
+		serverAPI = new ServerAPI();
+		this.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&6FreezeFight&7] &aLoading contents"));
+		loadCommands();
+		loadEvents();
+		loadFiles();
 
-public class FreezeFight extends JavaPlugin {
-    public FreezeFight instance;
-    private static ServerAPI serverAPI;
-    private static LocationInterface locationAPI;
-    public static String NAMESPACE = "FreezeFight";
-    public static String currentMap = null;
-    public static boolean inMode = false;
+		statsRepository = new StatsRepository(serverAPI.getConnectionSource());
+		statsInterface = statsRepository;
 
-    private StatsRepository statsRepository;
-    private StatsInterface statsInterface;
+		for(Player all : Bukkit.getOnlinePlayers()) {
+			PotionEffect speed = new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1, true, false);
+			PotionEffect jumpboost = new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 1, true, false);
+			all.addPotionEffect(speed);
+			all.addPotionEffect(jumpboost);
+		}
+	}
 
-    private LocationRepository locationRepository;
+	public void onDisable() {
+		this.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&6Paintball&7] &aPlugin disabled"));
+		this.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&6Paintball&7] &aDeveloped by &cKorzHorz"));
+	}
 
-     @Override
-     public void onLoad() {
-         instance = this;
-     }
+	public void loadCommands() {
+		this.getCommand("SetSpawn").setExecutor(new CMD_SetSpawn(this));
+		this.getCommand("Stats").setExecutor(new CMD_Stats(this));
+	}
+	
+	public void loadEvents() {
+		getServer().getPluginManager().registerEvents(new EVT_ItemDropEvent(this), this);
+		getServer().getPluginManager().registerEvents(new EVT_ItemPickupEvent(this), this);
+		getServer().getPluginManager().registerEvents(new EVT_PlayerDeathEvent(this), this);
+		getServer().getPluginManager().registerEvents(new EVT_PlayerJoinEvent(this), this);
+		getServer().getPluginManager().registerEvents(new EVT_PlayerQuitEvent(this), this);
+		getServer().getPluginManager().registerEvents(new EVT_PlayerMoveEvent(this), this);
+		getServer().getPluginManager().registerEvents(new EVT_FoodLevelChangeEvent(), this);
+		getServer().getPluginManager().registerEvents(new EVT_FallDamageEvent(), this);
+		getServer().getPluginManager().registerEvents(new EVT_EntityDamageByEntityEvent(), this);
+		getServer().getPluginManager().registerEvents(new EVT_ProjectileHitEvent(this), this);
+	}
 
-    @Override
-    public void onEnable() {
-        serverAPI = new ServerAPI();
-        int randonMap = new Random().nextInt(locationAPI.getAllKeys(NAMESPACE).size());
+	public void loadFiles() {
+		FreezeFight.locations = new File(this.getDataFolder(), "locations.yml");
+		if (this.getDataFolder().exists()) {
+			this.getDataFolder().mkdir();
+		}
 
-        currentMap = locationAPI.getAllKeys(NAMESPACE).get(randonMap);
+		if (!locations.exists()) {
+			try {
+				locations.createNewFile();
+			} catch (IOException exception) {
+				exception.printStackTrace();
+			}
+		}
+		FreezeFight.loc = YamlConfiguration.loadConfiguration(FreezeFight.locations);
+	}
 
-        statsRepository = new StatsRepository(serverAPI.getConnectionSource());
-        statsInterface = statsRepository;
-
-        locationRepository = new LocationRepository(serverAPI.getConnectionSource());
-        locationAPI = locationRepository;
-
-
-        if (locationAPI.getAllMaps().size() != 0) {
-            currentMap = locationAPI.getAllMaps().get(new Random().nextInt(locationAPI.getAllMaps().size()));
-            inMode = true;
-        } else {
-            inMode = false;
-        }
-
-        getCommand("setSpawn").setExecutor(new setSpawnLocaton());
-
-        PluginManager pluginManager = Bukkit.getPluginManager();
-
-        pluginManager.registerEvents(new ConnectionListener(), this);
-        pluginManager.registerEvents(new EventListener(), this);
-        pluginManager.registerEvents(new ProjectileListener(), this);
-        pluginManager.registerEvents(new DeathListener(statsInterface, locationAPI), this);
-    }
-
-    @Override
-    public void onDisable() {
-    }
-
-    public FreezeFight getInstance() {
-        return instance;
-    }
-
-    public static LocationInterface getLocationAPI() {
-        return locationAPI;
-    }
+	public static StatsInterface getStatsInterface() {
+		return statsInterface;
+	}
 }
--- src/main/java/de/chaos/mc/freezefight/utils/GameUtils.java
+++ src/main/java/de/chaos/mc/freezefight/utils/GameUtils.java
@@ -1,21 +1,10 @@
 package de.chaos.mc.freezefight.utils;
 
-import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.PlayerInventory;
 
 public class GameUtils {
-    public static void setInvItems(Player player) {
-        PlayerInventory inventory = player.getInventory();
-        inventory.clear();
-        inventory.setItem(0, new ItemBuilder(Material.IRON_SWORD).name("§6Schwert").unbreakable(true).itemStack());
-        inventory.setItem(1, new ItemBuilder(Material.BOW).name("§6Bogen").enchantment(Enchantment.ARROW_DAMAGE, 3).unbreakable(true).enchantment(Enchantment.ARROW_KNOCKBACK, 1).itemStack());
-        inventory.setItem(10, new ItemBuilder(Material.ARROW, 32).itemStack());
-        inventory.setItem(3, new ItemBuilder(Material.SNOW_BALL, 16).name("§6Frostball").itemStack());
-    }
     public static AttributeInstance setCountdown(Player player) {
         AttributeInstance attributeInstance = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
         attributeInstance.setBaseValue(10);
